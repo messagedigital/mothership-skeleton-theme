6c2a6ee54e83094af08d1cf9db404e5cb031812e
Samuel Trangmar-Keates <sam@message.co.uk>
2014-12-05 10:56:29 +0000

    App.php merge conflicts

--- app/App.php
+++ app/App.php
@@ -1,17 +1,10 @@
 <?php
-<<<<<<< HEAD
 
-=======
->>>>>>> e4c26e5b331de3bff71afe98a4fb42cc4efeec2f
 class App extends \Message\Cog\Application\Loader
 {
 	protected function _registerModules()
 	{
-<<<<<<< HEAD
-		return array(
-=======
 		return [
->>>>>>> e4c26e5b331de3bff71afe98a4fb42cc4efeec2f
 			'Message\\ImageResize',
 			'Message\\User',
 			'Message\\Mothership\\User',
@@ -21,15 +14,7 @@ class App extends \Message\Cog\Application\Loader
 			'Message\\Mothership\\Commerce',
 			'Message\\Mothership\\Ecommerce',
 			'Message\\Mothership\\OrderReturn',
-<<<<<<< HEAD
 			'Mothership\\Site'
-		);
-=======
-			'Message\\Mothership\\Voucher',
-			'Message\\Mothership\\Discount',
-			'Message\\Mothership\\Report',
-			'Mothership\\Site',
 		];
->>>>>>> e4c26e5b331de3bff71afe98a4fb42cc4efeec2f
 	}
 }
