4d8d6626045705ea3b9d8f1f8c98127e62a3ff2a
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-01-09 11:20:55 +0000

    remove ecommerce modules

--- app/App.php
+++ app/App.php
@@ -12,11 +12,6 @@ class App extends \Message\Cog\Application\Loader
 			'Message\\Mothership\\ControlPanel',
 			'Message\\Mothership\\FileManager',
 			'Message\\Mothership\\CMS',
-			'Message\\Mothership\\Commerce',
-			'Message\\Mothership\\Ecommerce',
-			'Message\\Mothership\\OrderReturn',
-			'Message\\Mothership\\Voucher',
-			'Message\\Mothership\\Discount',
 			'Message\\Mothership\\Report',
 			'Mothership\\Site',
 		];
