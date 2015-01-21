717b741f0ebaf6ce41cfa6a985d842f0e4f1e0b0
Aisling Brock <aisling@message.co.uk>
2015-01-21 11:53:46 +0000

    Reducing clicks in Mothership, changing CSS attributions

--- app/site/resources/assets/js/jquery.menu.js
+++ app/site/resources/assets/js/jquery.menu.js
@@ -1,7 +1,7 @@
 /**
  * Navigation pushdown menu
  *
- * This plugin corseponds with the navigation menu for Mothership sites
+ * This plugin corseponds with the navigation menu for Stoneham.
  *
  * This is a *private* plugin, and should only be used by Message Digital Design.
  *
@@ -13,63 +13,87 @@ jQuery(document).ready(function($) {
 
 	// Set all required variables
 	var navigation = $('.navigation'),
-		navLink    = navigation.find('ul > li > a'),
+		navLink    = navigation.find('ol > li > a'),
+		backLink   = $('.mobile-back'),
 		open 	   = false;
 
 	// Insert the subnavigation wrapper
-	navigation.after('<div class="subnav-wrapper"></div>');
+	navigation.after('<div class="subnav-wrapper nav-offcanvas"></div>');
 	var wrapper = $('.subnav-wrapper');
 
-	// Offset the wrapper
-	wrapper.css({
-		marginTop: '-56px'
+	backLink.css({
+		zIndex: '-9999'
 	});
 
 	// Click on main navigation
 	navLink.on('click', function() {
 
 		var self = $(this),
-			subnav = self.parent().find('.sub-navigation ul');
+			subnav = self.parent().find('.sub-navigation ol');
 
 		// Allow clicks on links without sub navigation
 		if(!self.siblings('.sub-navigation').length) {
 			return;
 		}
 
+		if($('.navigation li').hasClass('current')) {
+			$('.navigation li').removeClass('current')
+		}
+
 		// Open navigation
-		function openNav() {
-
-			open = true;
-
+		function openNav(callback) {
 			wrapper.html(subnav.clone());
+			wrapper.slideDown(500, callback);
+
 			wrapper.children().show();
 
-			wrapper.css({
-				marginTop: '0px'
-			});
+			open = self.data('pushid');
+
+			backLink.css({
+				zIndex: '999999'
+			})
+
 		}
 
 		// Close navigation
-		function closeNav() {
+		function closeNav(callback) {
+
+			wrapper.children('.sub-navigation').remove();
+
+			wrapper.slideUp(500, callback);
 
 			open = false;
 
-			wrapper.css({
-				marginTop: '-56px'
-			});
-
-			wrapper.children('.sub-navigation').remove();
 		}
 
 		// Open and close if statement
-		if (open == true) {
-			closeNav();
-		} else if (open == false) {
+		if (open !== false) {
+			if (open !== self.data('pushid')){
+				closeNav(openNav);
+				$('.navigation li a').removeClass('current');
+				self.addClass('current');
+			}else {
+				closeNav();
+				self.removeClass('current');
+			}
+		} else if (open === false) {
 			openNav();
+			self.addClass('current');
 		}
+
 
 		return false;
 
+	});
+
+	$('.mobile-back').on('click', function() {
+		wrapper.children('.sub-navigation').remove();
+		wrapper.slideUp(500);
+		open = false;
+
+		backLink.css({
+			zIndex: '-9999'
+		});
 	});
 
 });
