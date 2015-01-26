c9f6fe5f1dad660838d439c40023f1436b910e5c
Samuel Trangmar-Keates <sam@message.co.uk>
2015-01-26 16:15:27 +0000

    Revert "removed ecommerce stuff"
    This reverts commit 5f9b3aedc909c6a4a7c0ab7cfaf21a1872a03712.

--- app/App.php
+++ app/App.php
@@ -12,6 +12,11 @@ class App extends \Message\Cog\Application\Loader
 			'Message\\Mothership\\ControlPanel',
 			'Message\\Mothership\\FileManager',
 			'Message\\Mothership\\CMS',
+			'Message\\Mothership\\Commerce',
+			'Message\\Mothership\\Ecommerce',
+			'Message\\Mothership\\OrderReturn',
+			'Message\\Mothership\\Voucher',
+			'Message\\Mothership\\Discount',
 			'Message\\Mothership\\Report',
 			'Mothership\\Site',
 		];
