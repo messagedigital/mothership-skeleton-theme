7d256e261027e7b3c851c5aadc5b6d568233260d
Aisling Brock <aisling@message.co.uk>
2015-01-29 13:48:11 +0000

    checkout and accounts css changes

--- app/site/resources/assets/js/jquery.setup.js
+++ app/site/resources/assets/js/jquery.setup.js
@@ -57,4 +57,28 @@ jQuery(document).ready(function($) {
 			e.carousel('stop');
 		}
 	});
+
+	// Returns Toggle
+
+
+	$(function() {
+        var toggleReturnsInput = function() {
+            var val;
+            val = $('#form_resolution').val();
+
+            if ('refund' == val) {
+                $('#form_exchangeUnit').parents('.field').hide(0);
+            }
+            else {
+                $('#form_exchangeUnit').parents('.field').show(0);
+            }
+        };
+
+        $('#form_resolution').change(function() {
+            toggleReturnsInput();
+        });
+
+        toggleReturnsInput();
+    });
+    
 });
